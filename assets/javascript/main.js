0bc4471e9fdbbbb32e33ad18fdf518e400bd9744
andriyana <andriyana2212@gmail.com>
2018-11-12 21:50:52 -0500

    zomato initial info

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,14 +2,32 @@
 // API key - 495fd465b1df3b6ee70c8cd31b998836
 
 // AJAX specific for Zomato:
-// $.ajax({  
-//    url: "https://developers.zomato.com/api/v2.1/cities?q=new%20york", 
-//    dataType: 'json',
-//    async: true,
-//    beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
-//    '495fd465b1df3b6ee70c8cd31b998836');},  // This inserts the api key into the HTTP header
-//    success: function(response) 
-//    { console.log(response.location_suggestions) } });
+function display() {
+
+    $.ajax({
+           url: "https://developers.zomato.com/api/v2.1/search?lat=40.7648&lon=-73.9808&radius=1000", 
+           // the url above will need to be modified. We will need to pull the latitude and longitude from the map
+           method: "GET",
+           dataType: "json",
+           headers: {
+           "user-key": "495fd465b1df3b6ee70c8cd31b998836"
+           }
+         }).then(function(response) {
+           console.log(response);
+           console.log
+           var results = response.restaurants;
+           for (var i = 0; i < results.length; i++) {
+             console.log(results[i].restaurant.name)
+             console.log(results[i].restaurant.location.address)
+             console.log(results[i].restaurant.menu_url)
+             console.log(results[i].restaurant.cuisines)
+             console.log(results[i].restaurant.price_range)
+   
+           }
+       });
+     }
+   
+   display();
 
 // Google Geolocation / geocoding API Key: AIzaSyDfe8FcVBVkJX2yP6vNEyjLGyxsJ_oJMGI
 
