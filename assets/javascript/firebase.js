4d2f7e72398973d492ac33e1283bbd70e056d238
ant4u <anthonyimontalto@gmail.com>
2018-11-21 00:26:32 -0500

    Added google sign-in code

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,17 +1,49 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBMEhgfhFIrC_YYgYVR2a2eA86TgGmsTgA",
-    authDomain: "whereshappening-1541645493962.firebaseapp.com",
-    databaseURL: "https://whereshappening-1541645493962.firebaseio.com",
-    projectId: "whereshappening-1541645493962",
-    storageBucket: "whereshappening-1541645493962.appspot.com",
-    messagingSenderId: "538548590479"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBMEhgfhFIrC_YYgYVR2a2eA86TgGmsTgA",
+  authDomain: "whereshappening-1541645493962.firebaseapp.com",
+  databaseURL: "https://whereshappening-1541645493962.firebaseio.com",
+  projectId: "whereshappening-1541645493962",
+  storageBucket: "whereshappening-1541645493962.appspot.com",
+  messagingSenderId: "538548590479"
+};
+firebase.initializeApp(config);
 
+function login() {
+  function newLoginHappened(user) {
+    if (user) {
+      // User is signed in
+      app(user);
+    } else {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    }
+  }
 
+  firebase.auth().onAuthStateChanged(newLoginHappened);
+}
 
-  $(document).ready(function() {
-    //TODO: firebase logic goes here
+function signOut() {
+  firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      $(".userInfo").empty();
+    })
+    .catch(function(error) {
+      // An error happened.
+    });
+}
 
-  })
+function app(user) {
+  console.log(user);
+  var userText = $("<p>").text("Welcome ");
+  var uName = $("<span>").text(user.displayName + "!");
+  userText.append(uName);
+  $(".userInfo").append(userText);
+  // $("#clientName").html(user.displayName + "!");
+}
+
+$(document).ready(function() {
+  $("#loginLink").on("click",login);
+  $("#signoutLink").on("click",signOut);
+});
+
