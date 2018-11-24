8a3a1d4b89c0123007014d8df5422d480064a563
ant4u <anthonyimontalto@gmail.com>
2018-11-24 15:44:25 -0500

    more login fixes

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -10,30 +10,27 @@ var config = {
 firebase.initializeApp(config);
 
 function login() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+ 
   function newLoginHappened(user) {
     if (user) {
       // User is signed in
       app(user);
-    } else {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(provider);    
     }
   }
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-
-  $(".loginLink").css("display","none");
-  $(".signoutLink").css("display","block");
   firebase.auth().onAuthStateChanged(newLoginHappened);
-
-
+ 
 }
 
 function signOut() {
-  firebase.auth().signOut().then(function() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function() {
       // Sign-out successful.
-      $(".loginLink").css("display","block");
-      $(".signoutLink").css("display","none");
+      $(".loginLink").css("display", "block");
+      $(".signoutLink").css("display", "none");
       // clear "Welcome" area
     })
     .catch(function(error) {
@@ -43,15 +40,18 @@ function signOut() {
 
 function app(user) {
   console.log(user);
+  
+  $(".loginLink").css("display", "none");
+  $(".signoutLink").css("display", "block");
   // $(".loginLink").html("Welcome "+user.displayName+"! " + "<a href='#'>Sign out</a>");
   var welcomeText = $("<span>").text("Welcome! ");
   welcomeText.append(user.displayName);
-
 }
 
+
+
 $(document).ready(function() {
-  $(".loginLink").on("click",login);
-  $(".signoutLink").on("click",signOut);
-
+  $(".loginLink").on("click", login);
+  $(".signoutLink").on("click", signOut);
+  // firebase.auth().onAuthStateChanged(newLoginHappened);
 });
-
