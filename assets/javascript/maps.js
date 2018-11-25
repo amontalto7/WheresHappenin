bf0b25a9d2377ac7cbe3add23b4d72880b41eb2d
ant4u <anthonyimontalto@gmail.com>
2018-11-24 21:17:43 -0500

    removed popup when clicking on map

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -67,16 +67,6 @@ var eIcon = new customIcon({ iconUrl: "icons8-marker-40blue.png" });
 
 //-------------------------------------------------------------
 
-// MAP EVENTS (test by clicking on the map)
-var locationPopup = L.popup();
-
-function onMapClick(e) {
-  locationPopup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(mymap);
-}
-
 mymap.on("click", onMapClick);
 
 // URL to look up Latitude & Longitude of an address
