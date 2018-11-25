13dc4afa7e999fba7afdd78778fe89f7d3cbb220
ant4u <anthonyimontalto@gmail.com>
2018-11-24 22:43:56 -0500

    fixed login flow issues

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -11,16 +11,18 @@ firebase.initializeApp(config);
 
 function login() {
   var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope("profile");
+  provider.addScope("email");
+
   firebase.auth().signInWithRedirect(provider);
- 
-  function newLoginHappened(user) {
-    if (user) {
-      // User is signed in
-      app(user);
-    }
-  }
-  firebase.auth().onAuthStateChanged(newLoginHappened);
- 
+
+  // function newLoginHappened(user) {
+  //   if (user) {
+  //     // User is signed in
+  //     app(user);
+  //   }
+  // }
+  // firebase.auth().onAuthStateChanged(newLoginHappened);
 }
 
 function signOut() {
@@ -38,20 +40,69 @@ function signOut() {
     });
 }
 
-function app(user) {
-  console.log(user);
-  
-  $(".loginLink").css("display", "none");
-  $(".signoutLink").css("display", "block");
-  // $(".loginLink").html("Welcome "+user.displayName+"! " + "<a href='#'>Sign out</a>");
-  var welcomeText = $("<span>").text("Welcome! ");
-  welcomeText.append(user.displayName);
-}
-
-
-
 $(document).ready(function() {
   $(".loginLink").on("click", login);
   $(".signoutLink").on("click", signOut);
   // firebase.auth().onAuthStateChanged(newLoginHappened);
 });
+
+
+
+function app(user) {
+  console.log(user);
+
+  $(".loginLink").css("display", "none");
+  $(".signoutLink").css("display", "block");
+  // $(".loginLink").html("Welcome "+user.displayName+"! " + "<a href='#'>Sign out</a>");
+  var fullName = user.displayName;
+  var splitName = fullName.split(" ");
+
+  var welcomeText = $("<span>").text("Welcome! ");
+  welcomeText.append(splitName[0]);
+  $(".userWelcome").append(welcomeText)
+}
+
+
+function checkLoginStatus() {
+
+  function newLoginHappened(user) {
+    if (user) {
+      // User is signed in
+      app(user);
+    } else {
+      $(".loginLink").css("display", "block");
+      $(".signoutLink").css("display", "none");
+    }
+  }
+
+
+  firebase.auth().onAuthStateChanged(newLoginHappened);
+  // firebase.auth().getRedirectResult().then(function(result) {
+  //     if (result.credential) {
+  //       // This gives you a Google Access Token.
+  //       var token = result.credential.accessToken;
+  //       console.log(token);
+  //     }
+  //     var user = result.user;
+  //     console.log("User info:");
+  //     console.log(user);
+  //   })
+  //   .catch(function(error) {
+  //     // Handle Errors here.
+  //     var errorCode = error.code;
+  //     var errorMessage = error.message;
+  //     // The email of the user's account used.
+  //     var email = error.email;
+  //     // The firebase.auth.AuthCredential type that was used.
+  //     var credential = error.credential;
+  //     console.log(errorCode);
+  //     console.log(errorMessage);
+  //     console.log(email);
+  //     console.log(credential);
+  //   });
+
+
+
+}
+
+window.onload = checkLoginStatus;
